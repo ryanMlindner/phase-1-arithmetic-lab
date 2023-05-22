eb80500536960b7d0b5906502205642d9ce5ef99
Ryan Lindner <ryan.m.lindner@gmail.com>
2023-05-22 11:10:35 -0600

    lab completed

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random()* 10) + 1;
+const num3 = 9;
+const num4 = 5;
+let mod = num3 % num4;
+let max = Math.max(1,7,20,3);
